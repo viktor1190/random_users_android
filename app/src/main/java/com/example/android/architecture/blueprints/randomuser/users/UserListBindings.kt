2e787840670b72c5576260d391aa852176f4e13b
Victor Valencia <cytrux@gmail.com>
2020-05-08 14:24:19 -0500

    Adding Glide lib to load the profile pictures in list
    Signed-off-by: Victor Valencia <cytrux@gmail.com>

--- app/src/main/java/com/example/android/architecture/blueprints/randomuser/users/UserListBindings.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/randomuser/users/UserListBindings.kt
@@ -1,22 +1,9 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.architecture.blueprints.randomuser.users
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.android.architecture.blueprints.randomuser.data.User
 
 /**
@@ -25,4 +12,11 @@ import com.example.android.architecture.blueprints.randomuser.data.User
 @BindingAdapter("app:items")
 fun setItems(listView: RecyclerView, items: List<User>) {
     (listView.adapter as UsersAdapter).submitList(items)
+}
+
+@BindingAdapter("app:profileImage")
+fun loadImage(view: ImageView, imageUrl: String) {
+    Glide.with(view.context)
+            .load(imageUrl)
+            .into(view)
 }
